2c9c2416dbd7deb72f9866c5e97180e647226d3f
Mohak Chavan <mohak44chavan@gmail.com>
2021-04-07 19:18:07 +0530

    Minor Bug resolved.

--- src/main/java/com/mohakchavan/pustakniparab_web/MainServlets/ReturnIssue.java
+++ src/main/java/com/mohakchavan/pustakniparab_web/MainServlets/ReturnIssue.java
@@ -96,7 +96,7 @@ public class ReturnIssue extends HttpServlet {
 
 			} else {
 			    //Some error
-			    request.setAttribute(Constants.ATTRIBUTE_KEY_NAMES.HAS_TRNSCTN_ERROR_WITH_DATA, Constants.ERRORS.ISSUE_NOT_ADDED);
+			    request.setAttribute(Constants.ATTRIBUTE_KEY_NAMES.HAS_TRNSCTN_ERROR_WITH_DATA, Constants.ERRORS.ISSUE_NOT_RETURNED);
 			    latch.countDown();
 			}
 		    }
