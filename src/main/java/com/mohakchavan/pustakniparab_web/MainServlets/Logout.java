bbf110f7b4ecdb032159806eda9a99abc0cd90fb
Mohak Chavan <mohak44chavan@gmail.com>
2021-05-14 15:37:42 +0530

    Small change done.

--- src/main/java/com/mohakchavan/pustakniparab_web/MainServlets/Logout.java
+++ src/main/java/com/mohakchavan/pustakniparab_web/MainServlets/Logout.java
@@ -9,7 +9,6 @@ import com.mohakchavan.pustakniparab_web.StaticClasses.Constants;
 import java.io.IOException;
 import java.io.PrintWriter;
 import javax.servlet.ServletException;
-import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -19,7 +18,6 @@ import javax.servlet.http.HttpSession;
  *
  * @author Mohak Chavan
  */
-@WebServlet(name = "Logout", urlPatterns = {"/logout"})
 public class Logout extends HttpServlet {
 
     /**
